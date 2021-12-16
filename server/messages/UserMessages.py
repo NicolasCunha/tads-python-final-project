9ea71dbaba6aa97f0de916465ebe8c985134d28c
NicolasCunha <nicolascunha17@gmail.com>
2021-12-16 19:55:16 -0300

    feat: add messages module

---
+++ server/messages/UserMessages.py
@@ -0,0 +1,4 @@
+USER_LOGIN_NOT_EMPTY = "User login can't be empty"
+USER_PWD_NOT_EMPTY = "User password can't be empty"
+USER_EXISTS = "Email {email} or user {user} already exists"
+VALUE_NOT_FOUND_OR_EMPTY = "Value {key} not found or is an empty value."
